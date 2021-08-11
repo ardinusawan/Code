17ba257aa40f1956e6241c2780f4bbfdc7eda7ec
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2021-08-11 21:27:27 +0800

    Update 646. Rotate Array.py

--- LeetCode/Top Interview Questions/646. Rotate Array.py
+++ LeetCode/Top Interview Questions/646. Rotate Array.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k = k%len(nums)
+        right = nums[-k:]
+        left = nums[:len(nums)-k]
+        new = right + left
+        # Somehow (new = right + left) not work!
+        # So I iterate it 
+        for i, n in enumerate(nums):
+            nums[i] = new[i]
+        

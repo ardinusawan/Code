fdeafb64eaf7111e70e2c78f0bd8c410b98f6947
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2021-08-11 21:27:42 +0800

    Update 646. Rotate Array.py

--- LeetCode/Top Interview Questions/646. Rotate Array.py
+++ LeetCode/Top Interview Questions/646. Rotate Array.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         k = k%len(nums)

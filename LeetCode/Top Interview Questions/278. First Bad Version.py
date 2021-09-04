41a047d9c433d1c19793ad6f7fca6155629ea237
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2021-09-04 22:56:39 +0800

    Update 278. First Bad Version.py

--- LeetCode/Top Interview Questions/278. First Bad Version.py
+++ LeetCode/Top Interview Questions/278. First Bad Version.py
@@ -4,47 +4,22 @@
 # def isBadVersion(version):
 
 class Solution:
-    result = 0
     def firstBadVersion(self, n):
-        # global result
-        # result = n
         """
         :type n: int
         :rtype: int
         """
-        l = 1
-        h = n
-        m = n
+        left = 1
+        right = n
+        mid = 1
         result = n
-        
-        while(l<=h):
-            m = int(l + (h-l)/2)
-            print(m)
-            if(isBadVersion(m)):
-                result = m
-                h = m -1
+        while(left<right):
+            mid = int(left + (right-left)/2)
+            if isBadVersion(mid):
+                result = mid
+                right = mid
             else:
-                l = m +1
+                left = mid +1
         return result
-        # self.find(1, n, int((n-1)/2+1))
-        # return result
-        
-    # def find(self, l, h, m):
-    #     global result
-    #     if isBadVersion(m):
-    #         l = l+1
-    #         h = m
-    #         m = int(l + (h-l)/2)
-    #     else:
-    #         l = m
-    #         h = h
-    #         m = int(l + (h-l)/2)
-    #     if l == m == h:
-    #         result = m
-    #         print(m)
-    #         return m
-    #     else:
-    #         self.find(l, h, m)
-        
-        
+            
         

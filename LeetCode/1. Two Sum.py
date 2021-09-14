c436f064a20c9bad3d414aaa37ecf27e12ef32d8
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2021-09-14 22:32:31 +0800

    Update 1. Two Sum.py

--- LeetCode/1. Two Sum.py
+++ LeetCode/1. Two Sum.py
@@ -1,19 +1,13 @@
-# https://leetcode.com/problems/two-sum/
-# https://leetcode.com/submissions/detail/259602094/
+# Problem: https://leetcode.com/problems/two-sum/
+# Detail: https://leetcode.com/submissions/detail/554808992/
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        temp = {}
-        for v in nums:
-            key = target - v
-            value = v
-            temp[key] = value
-        result = []
-        for i,v in enumerate(nums):
-            if v == target/2 and nums.count(v) == 1:
-                pass
-            else:
-                if v in temp.keys():
-                    result.append(i)
+        m = {}
+        r = []
+        for i, n in enumerate(nums):
+            if target-n in m:
+                r = [m[target-n], i]
+            m[n] = i
+        return r
         
-        return result

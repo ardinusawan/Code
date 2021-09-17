872653504c6f1944e73d1bd47fded95bcd69ae49
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2021-09-18 00:54:30 +0800

    Create 70. Climbing Stairs.java

---
+++ LeetCode/Top Interview Questions/70. Climbing Stairs.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class Solution {
+    private HashMap<Integer, Integer> memo = new HashMap<>();
+    
+    public int climbStairs(int n) {
+        //pattern is fibonacci
+        return calculate(n);
+    }
+    
+    private int calculate(int n) {
+        if(n <= 2) {
+            return n;
+        }
+        
+        if(memo.containsKey(n)) return memo.get(n);
+        
+        int result = 0;
+        result = calculate(n-1) + calculate(n-2);
+        memo.put(n, result);
+        return result;
+    }
+}

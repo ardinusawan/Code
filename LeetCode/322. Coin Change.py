34ba53d3a02daa74ad3cee2bece5a728134e898d
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2021-09-29 00:21:39 +0800

    Create 322. Coin Change.py

---
+++ LeetCode/322. Coin Change.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/coin-change/
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1] * (amount+1)
+        dp[0] = 0
+        for a in range(1, amount+1):
+            for c in coins:
+                if a >= c:
+                    dp[a] = min(dp[a], 1+dp[a-c])
+        return dp[amount] if dp[amount] != amount+1 else -1
